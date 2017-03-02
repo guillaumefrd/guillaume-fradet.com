3129c06fcb5b443b5444b6c6bf0acd7d7a8def10
Guillaume Fradet <Guillaume Fradet>
2017-03-02 14:49:41 +0100

    explore

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        jsonPretty = syntaxHighlight(jsonPretty);
         document.getElementById("info").innerHTML = jsonPretty;
     }
 };
@@ -15,6 +16,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        jsonPretty = syntaxHighlight(jsonPretty);
         document.getElementById("blockchaininfo").innerHTML = jsonPretty;
     }
 };
@@ -26,6 +28,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        jsonPretty = syntaxHighlight(jsonPretty);
         document.getElementById("mininginfo").innerHTML = jsonPretty;
     }
 };
@@ -37,8 +40,32 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        jsonPretty = syntaxHighlight(jsonPretty);
         document.getElementById("peerinfo").innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET", "http://bitcoin.mubiz.com/peerinfo", true);
 xmlhttp.send();
+
+
+
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
