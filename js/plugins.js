6cfbbe98fc175d6d57876b24818fe4ae6b725fa6
guillaumefrd <guillaume-fradet@hotmail.fr>
2017-10-31 11:43:52 -0500

    skills

---
+++ js/plugins.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  /* Drawbar */
+  $('.barres li').each(function() {
+  	var pourcentage = $(this).attr('data-skills');
+  	$(this).append($("<span></span>").animate({
+  		width : ''+ pourcentage +'%' }, 2000));
+  });
+
+});
