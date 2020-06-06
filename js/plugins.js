e9192d82d967226c36faa953899d751dc5d5b423
guillaumefrd <guillaume-fradet@hotmail.fr>
2020-06-06 16:00:26 +0200

    Update skills

--- js/plugins.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  /* Drawbar */
-  $('.barres li').each(function() {
-  	var pourcentage = $(this).attr('data-skills');
-  	$(this).append($("<span></span>").animate({
-  		width : ''+ pourcentage +'%' }, 2000));
-  });
-});
